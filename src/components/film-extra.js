a3fef4ca59a9273d0b396b2ccfbeb503514765a7
Sergey <st@ttsgroup.su>
2019-11-27 19:34:25 +0300

    cinema m2-t1

---
+++ src/components/film-extra.js
@@ -0,0 +1,5 @@
+export const getExtraFilmList = () => `<section class="films-list--extra">
+      <h2 class="films-list__title"></h2>
+
+      <div class="films-list__container"></div>
+    </section>`;
