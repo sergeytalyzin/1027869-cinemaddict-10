d9a032430d169d9d26bc54975fb62c7ee02244a8
Sergey <st@ttsgroup.su>
2020-01-11 05:12:27 +0300

    поправил

--- src/components/sorting.js
+++ src/components/sorting.js
@@ -5,10 +5,10 @@ export const SortType = {
   DEFAULT: `default`
 };
 
-export const getSortTemplate = () => `<ul class="sort">
-    <li><a href="#" data-sort-type ="${SortType.DEFAULT}"  class="sort__button sort__button--active">Sort by default</a></li>
-    <li><a href="#" data-sort-type ="${SortType.DATE}" class="sort__button">Sort by date</a></li>
-    <li><a href="#" data-sort-type ="${SortType.RATING}" class="sort__button">Sort by rating</a></li>
+const getSortTemplate = (obj) => `<ul class="sort">
+    <li><a href="#" data-sort-type ="${obj.DEFAULT}"  class="sort__button sort__button--active">Sort by default</a></li>
+    <li><a href="#" data-sort-type ="${obj.DATE}" class="sort__button">Sort by date</a></li>
+    <li><a href="#" data-sort-type ="${obj.RATING}" class="sort__button">Sort by rating</a></li>
   </ul>`;
 
 export default class Sorting extends AbstractComponent {
@@ -17,7 +17,7 @@ export default class Sorting extends AbstractComponent {
     this._currenSortType = SortType.DEFAULT;
   }
   getTemplate() {
-    return getSortTemplate();
+    return getSortTemplate(SortType);
   }
   setSortTypeHandler(handler) {
     this._element.addEventListener(`click`, (evt)=> {
