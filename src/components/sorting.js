4ca58c79710649919f60739137a42e0f624bbd3c
Sergey <st@ttsgroup.su>
2020-01-11 17:34:00 +0300

    исправил сортировку

--- src/components/sorting.js
+++ src/components/sorting.js
@@ -5,23 +5,32 @@ export const SortType = {
   DEFAULT: `default`
 };
 
-const getSortTemplate = (obj) => `<ul class="sort">
-    <li><a href="#" data-sort-type ="${obj.DEFAULT}"  class="sort__button sort__button--active">Sort by default</a></li>
-    <li><a href="#" data-sort-type ="${obj.DATE}" class="sort__button">Sort by date</a></li>
-    <li><a href="#" data-sort-type ="${obj.RATING}" class="sort__button">Sort by rating</a></li>
+const sortType = Object.values(SortType);
+
+const getSortTemplate = () => `<ul class="sort">
+    ${createSort(sortType)}
   </ul>`;
+const createSort = (array) => {
+  return [...array].map((it) => {
+    return (`<li><a href="#" data-sort-type ="${it}"  class="sort__button">Sort by ${it}</a></li>`);
+  }).join(`\n`);
+};
 
 export default class Sorting extends AbstractComponent {
   constructor() {
     super();
     this._currenSortType = SortType.DEFAULT;
+    this.save = null;
   }
   getTemplate() {
-    return getSortTemplate(SortType);
+    return getSortTemplate();
   }
   setSortTypeHandler(handler) {
     this._element.addEventListener(`click`, (evt)=> {
       evt.preventDefault();
+      if (this.save && this.save !== evt.target) {
+        this.save.classList.remove(`sort__button--active`);
+      }
       if (evt.target.tagName !== `A`) {
         return;
       }
@@ -30,6 +39,8 @@ export default class Sorting extends AbstractComponent {
         return;
       }
       this._currenSortType = sortType;
+      evt.target.classList.add(`sort__button--active`);
+      this.save = evt.target;
       handler(this._currenSortType);
     });
   }
