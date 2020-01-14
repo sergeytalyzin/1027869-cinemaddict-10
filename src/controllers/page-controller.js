88a9db1457d991264bb58c7afefac9a67c656200
Sergey <st@ttsgroup.su>
2020-01-14 15:08:01 +0300

    module5-task2 прими пожалуйста тоже сделано в предыдущей ветке

--- src/controllers/page-controller.js
+++ src/controllers/page-controller.js
@@ -13,7 +13,6 @@ const CARDS_INDICATOR = 5;
 const CARDS_INDICATOR_EXTRA = 2;
 const CATEGORIES_NUMBER = 2;
 const CARD_BUTTON = 5;
-
 const ESCAPE_KEY = 27;
 
 const siteBody = document.querySelector(`body`);
